809e733848b39c500197096f04a2c87a29192f3e
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-18 10:09:57 +0300

    Initial Commit

---
+++ MyReminders/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyReminders
+//
+//  Created by Никита Коголенок on 18.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
